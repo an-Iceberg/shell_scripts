6808ed54c7688437eb046b21fe900925625f2305
unknown <a.g.marosi@bluewin.vh>
2022-05-11 22:35:33 +0200

    improved input handling of clock arms degree script

--- c_a_d.sh
+++ c_a_d.sh
@@ -1,15 +1,12 @@
 #! /usr/bin/bash
 
-# Clock arms degree calculator
 # This script calculates the degrees between the two clock arms given a time
+echo;
+echo "#----------------------------------------------------------------------------#";
+echo "| This script calculates the degrees between the two clock arms given a time |";
+echo "#----------------------------------------------------------------------------#";
 
-if (($# < 2))
-then
-  echo "Hour and Minute parameters must be added."
-  exit
-fi
-
-# Returns the absolute difference between two inputs
+# Returns the absolute difference between two given numbers
 function diff()
 {
   if (($1 > $2))
@@ -20,13 +17,68 @@ function diff()
   return $(($2 - $1));
 }
 
-# $1 is hours, $2 is minutes
+isInputGood=true;
 
-degreeMinutes=$(($2 * 6));
-degreeHours=$(($1 * 30 + ($degreeMinutes / 72 * 6)));
+# Two arguments were passed to the script
+if (($# == 2))
+then
+  # Inputs are in unacceptable range
+  if (($1 < 0 || $1 > 12 || $2 < 0 || $2 > 60))
+  then
+    isInputGood=false;
+  fi
 
+# Wrong number of arguments was passed to the script
+else
+  isInputGood=false;
+fi
+
+# Determines what action to take based on the given inputs
+case $isInputGood
+in
+  # Input is correct
+  true )
+    hours=$1;
+    minutes=$2;
+  ;;
+
+  # Both minutes and hours are incorrect
+  false )
+    echo;
+
+    # User input for hours
+    while true;
+    do
+      read -p "Please enter the hour: " hours
+
+      # The amount of hours is in the accepted range
+      if (($hours >= 0 && $hours < 12))
+      then
+        break;
+      fi
+    done
+
+    # User input for minutes
+    while true;
+    do
+      read -p "Please enter the minutes: " minutes
+
+      # The amount of minutes is in the acceptable range
+      if (($minutes >= 0 && $minutes < 60))
+      then
+        break;
+      fi
+    done
+  ;;
+esac
+
+# Calculates the degrees of each clock arm
+degreeMinutes=$(($minutes * 6));
+degreeHours=$(($hours * 30 + ($degreeMinutes / 72 * 6)));
+
+# Calculates the abslute difference between the two clock arms
 diff $degreeMinutes $degreeHours;
 degrees=$?;
 
-#echo "$degreeMinutes, $degreeHours";
-echo "The degree between the short arm and the long arm at $1:$2 is $degrees degrees.";
+echo;
+echo "The degree between the short arm and the long arm at $hours:$minutes is $degrees degrees.";
